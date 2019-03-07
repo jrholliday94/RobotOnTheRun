3fa566f57a7368374fb14dc3dd76b0cf95a36bc3
jrholliday <jaredrholliday@gmail.com>
2019-03-07 16:09:16 -0600

    Added test scene for instantiate
    Test scene and script to demonstrate instantiate

---
+++ Robotontherun/Assets/scripts/Instantiate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour
+{
+    public Transform myPrefab;
+
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(myPrefab, new Vector3(i * 1.0F, 0, 0), Quaternion.identity);
+        }
+    }
+
+}
